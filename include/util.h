e69a8ceda5a663d713b1927254f17aecf95a4992
Didiet Noor <lynxluna@gmail.com>
2020-04-28 16:12:10 +0700

    Adding array of pointer buffer to handle mount

--- include/util.h
+++ include/util.h
@@ -1,15 +1,16 @@
 #ifndef KOSTAK_UTIL_H
 #define KOSTAK_UTIL_H 1
 
-/* 
- * Simple string tuple implementation
- * It's just an array of NULL-delimited string
- */
+typedef struct ptr_vec {
+  void **ptrs;
+  size_t count;
+  size_t cap;
+} ptr_vec_t;
 
-typedef struct strtuple {
-  char *buf;
-  char **bufarr;
-  
-} strtuple_t;
+static const size_t INIT_SIZE = 256;
+
+void init_ptr_vec(ptr_vec_t *vec);
+int add_ptr(ptr_vec_t *vec, void *ptr);
+void free_ptr_vec(ptr_vec_t *vec);
 
 #endif /* KOSTAK_UTIL_H */
---
+++ src/util.c
@@ -0,0 +1,38 @@
+#include "common.h"
+#include "util.h"
+
+void init_ptr_vec(ptr_vec_t *vec)
+{
+  memset(vec, 0, sizeof(ptr_vec_t));
+
+  vec->ptrs = mmap(0, INIT_SIZE * sizeof(void*), PROT_WRITE | PROT_READ, 
+    MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);  
+  vec->cap = INIT_SIZE;
+  vec->count = 0;
+}
+
+int add_ptr(ptr_vec_t *vec, void *ptr)
+{
+  if (vec->cap == vec->count) {
+    const size_t newcap = vec->cap + INIT_SIZE;
+    void *newbuf = mremap(vec->ptrs, vec->cap * sizeof(void*),
+      newcap * sizeof(void*), MREMAP_MAYMOVE);
+
+    if (NULL == newbuf) {
+      log_error("Failed when remapping buffer: %m");
+      return -1;
+    }
+
+    vec->cap = newcap;
+    vec->ptrs = newbuf;
+  }
+
+  vec->ptrs[vec->count++] = ptr;
+  return 0;
+}
+
+void free_ptr_vec(ptr_vec_t *vec) 
+{
+  munmap(vec->ptrs, vec->cap * sizeof(void*));
+  memset(vec, 0, sizeof(ptr_vec_t));
+}
