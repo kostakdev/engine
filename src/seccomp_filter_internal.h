c6a1534bedf9f092055168b3dce97feabf9a089d
Didiet Noor <lynxluna@gmail.com>
2020-06-03 11:39:36 +0700

    Add copyright notices on all files

--- src/seccomp_filter_internal.h
+++ src/seccomp_filter_internal.h
@@ -1,3 +1,22 @@
+/* * Kostak - A little container engine
+ * Copyright (C) 2020 Didiet Noor <dnoor@ykode.com>
+ *
+ * This file is part of Kostak.
+ *
+ * Kostak is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * Kostak is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Kostak.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef SECCOMP_FILTER_INTERNAL
 #define SECCOMP_FILTER_INTERNAL 1
 
